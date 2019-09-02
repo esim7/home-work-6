74832ff890fd5a38eb667d03e7bdd3d219d94429
esim7 <makishev@yahoo.com>
2019-09-02 13:20:19 +0600

    загрузка дз6

---
+++ home-work-6/Massiv.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace home_work_6
+{
+    public class Massiv
+    {
+        public int _size { get; set; }
+        private int[] _massiv;
+
+        public int this[int i]
+        {
+            get
+            {
+                return _massiv[i];
+            }
+            set
+            {
+                _massiv[i] = (int)Math.Pow(value, 2);
+            }
+        }
+
+        public Massiv()
+        {
+            _size = 1;
+            _massiv = new int[_size];
+        }
+
+        public Massiv(int size)
+        {
+            _size = size;
+            _massiv = new int[_size];
+        }
+    }
+}
---
+++ home-work-6/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace home_work_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Massiv massiv = new Massiv(3);
+            for(int i = 0; i < massiv._size; i++)
+            {
+                massiv[i] = i + 1;
+                Console.WriteLine(massiv[i]);
+            }
+            //for (int i = 0; i < massiv._size; i++)
+            //{
+                
+            //}
+            Console.ReadKey();
+        }
+    }
+}
